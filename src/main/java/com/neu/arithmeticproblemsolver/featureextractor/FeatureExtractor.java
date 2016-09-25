6dd6f3ebb6a9f0597f70fae56c53da3b60339861
vishalrajpal <vishalarajpal@gmail.com>
2016-09-25 16:22:53 -0700

    Initial Commit

---
+++ src/main/java/com/neu/arithmeticproblemsolver/featureextractor/FeatureExtractor.java
@@ -0,0 +1,13 @@
+package com.neu.arithmeticproblemsolver.featureextractor;
+
+/**
+ * Extracts the frequencies of n-gram sub pattern features for each label in the dataset.
+ */
+public class FeatureExtractor {
+
+
+
+    public static void main(final String[] args) {
+
+    }
+}
